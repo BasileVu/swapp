387490150cf291671b26d888153380212486485a
Mathieu Urstein <mathieu.urstein@gmail.com>
2016-10-12 10:56:51 +0200

    Changement de nom de projet pour swapp + ajout du gitignore

--- swapp/urls.py
+++ swapp/urls.py
@@ -1,4 +1,4 @@
-"""HEIG_PDG_2016 URL Configuration
+"""swapp URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.10/topics/http/urls/
--- swapp/wsgi.py
+++ swapp/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for HEIG_PDG_2016 project.
+WSGI config for swapp project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "HEIG_PDG_2016.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "swapp.settings")
 
 application = get_wsgi_application()
