b07b08536a4146bbea2f0a28611c629bfa426a3f
servietsky777 <antoine.drabble@gmail.com>
2016-11-03 00:40:28 +0100

    Start to fix item creation

--- items/views.py
+++ items/views.py
@@ -9,6 +9,7 @@ import datetime
 from django.urls import reverse
 
 from items.models import Category, Item
+from users.models import UserProfile
 
 
 @login_required(login_url="users:login", redirect_field_name="")
@@ -24,19 +25,23 @@ def create_view(request):
     except KeyError:
         return render(request, "items/create.html", {'categories': Category.objects.all()})
     if price_min > price_max:
-        return render(request, "users/register.html", {
+        return render(request, "users/create.html", {
+            'categories': Category.objects.all(),
             "error_message": "Price min is higher than price max."
         })
 
     try:
-        item = User.objects.create_user(name, description, price_min, price_max, creation_date, archived, category, request.user.id)
+        category = Category.objects.get(id=category)
+        item = Item.objects.create(name=name, description=description, price_min=price_min, price_max=price_max,
+                                   creation_date=creation_date, archived=archived,
+                                   owner=UserProfile.objects.get(user=request.user))
     except IntegrityError:
         return render(request, "items/create.html", {
             'categories': Category.objects.all(),
             "error_message": "Item already exists."
         })
 
-    item(request, item)
+    item(request)
     return HttpResponseRedirect(reverse("items:item"))
 
 
