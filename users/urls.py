2e7e9b9611d12d3c26a32db11f4936c65ab4d161
Basile Vu <basile.vu@gmail.com>
2016-11-02 23:54:51 +0100

    User API draft

--- users/urls.py
+++ users/urls.py
@@ -8,4 +8,8 @@ urlpatterns = [
     url(r"^login/$", views.login_view, name="login"),
     url(r"^logout/$", views.logout_view, name="logout"),
     url(r"^account/$", views.account_view, name="account"),
+    url(r"^api/login/$", views.api_login, name="api_login"),
+    url(r"^api/logout/$", views.api_logout, name="api_logout"),
+    url(r"^api/account/$", views.create_account, name="api_account_creation"),
+    url(r"^api/personal/$", views.get_personal_account_info, name="api_personal_info"),
 ]
