8716c46a1171a6ccb1947e80a9122aa8d53a1aa5
antoine drabble <antoine drabble>
2016-11-02 19:19:29 +0100

    Add the url to the items module

--- items/urls.py
+++ items/urls.py
@@ -2,10 +2,8 @@ from django.conf.urls import url
 
 from . import views
 
-app_name = "users"
+app_name = "items"
 urlpatterns = [
-    url(r"^register/$", views.register_view, name="register"),
-    url(r"^login/$", views.login_view, name="login"),
-    url(r"^logout/$", views.logout_view, name="logout"),
-    url(r"^account/$", views.account_view, name="account"),
+    url(r"^item/create/$", views.create_view, name="create"),
+    url(r"^item/$", views.item_view, name="item"),
 ]
--- items/views.py
+++ items/views.py
@@ -1,7 +1,16 @@
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.db import IntegrityError
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 import datetime
 
 # Create your views here.
+from django.urls import reverse
+
+from items.models import Category, Item
+
+
 @login_required(login_url="users:login", redirect_field_name="")
 def create_view(request):
     try:
@@ -10,27 +19,27 @@ def create_view(request):
         price_min = request.POST["price_min"]
         price_max = request.POST["price_max"]
         creation_date = datetime.datetime.now().time()
-        archived = false
+        archived = 0
         category = request.POST["category"]
     except KeyError:
-        return render(request, "items/create.html", {'categories':Category.objects.all()})
+        return render(request, "items/create.html", {'categories': Category.objects.all()})
     if price_min > price_max:
         return render(request, "users/register.html", {
             "error_message": "Price min is higher than price max."
         })
 
     try:
-        item = User.objects.create_user(name, description, price_min, price_max, creatiopn_date, archived, category, request.user.id)
+        item = User.objects.create_user(name, description, price_min, price_max, creation_date, archived, category, request.user.id)
     except IntegrityError:
         return render(request, "items/create.html", {
-            'categories' : Category.objects.all(),
+            'categories': Category.objects.all(),
             "error_message": "Item already exists."
         })
 
     item(request, item)
     return HttpResponseRedirect(reverse("items:item"))
 
-# Create your views here.
-@login_required(login_url="users:login", redirect_field_name="")
+
 def item_view(request):
-    return render(request, "item/item.html", {"item": item})
+    return render(request, "item/item.html", {"item": ""})
+
