1167ad1479a4b41b3ce613e46d1efe6aea626941
servietsky777 <antoine.drabble@gmail.com>
2016-11-14 20:23:22 +0100

    Add views for categories, likes and images

--- items/urls.py
+++ items/urls.py
@@ -4,16 +4,18 @@ from rest_framework import routers
 from . import views
 
 router = routers.DefaultRouter()
-router.register(r'items', views.ItemList)
-router.register(r'items', views.ItemDetail)
+router.register(r'api/items', views.ItemViewSet)
+router.register(r'api/categories', views.CategoryViewSet)
+router.register(r'api/likes', views.LikeViewSet)
+router.register(r'api/images', views.ImageViewSet)
 
 app_name = "items"
 urlpatterns = [
     url(r"^items/create/$", views.create_view, name="create_view"),
     url(r"^items/(?P<item_id>\d+)/$", views.item_view, name="item_view"),
-    url(r"^api/items/$", views.create_item, name="create_item"),
-    url(r"^api/items/(?P<item_id>\d+)/$", views.get_item, name="get_item"),
-    url(r"^api/items/(?P<item_id>\d+)/archive$", views.archive_item, name="archive_item"),
-    url(r"^api/items/(?P<item_id>\d+)/unarchive$", views.unarchive_item, name="unarchive_item"),
+    #url(r"^api/items/$", views.create_item, name="create_item"),
+    #url(r"^api/items/(?P<item_id>\d+)/$", views.get_item, name="get_item"),
+    #url(r"^api/items/(?P<item_id>\d+)/archive$", views.archive_item, name="archive_item"),
+    #url(r"^api/items/(?P<item_id>\d+)/unarchive$", views.unarchive_item, name="unarchive_item"),
     url(r'^', include(router.urls)),
 ]
