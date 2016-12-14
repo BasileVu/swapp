8a30ef29a763568d8d1d3defbac89598a7381120
sebastien_boson_heigvd <sebastien.boson@heig-vd.ch>
2016-12-14 10:59:00 +0100

    notification when a offer is accepted or refused

--- notifications/tests.py
+++ notifications/tests.py
@@ -4,10 +4,13 @@ from django.contrib.auth.models import User
 from django.test import TestCase
 
 from items.models import Category, Item
-from notifications.models import Notification, OfferNotification
+from notifications.models import Notification, OfferNotification, NewOfferNotification, AcceptedOfferNotification, \
+    RefusedOfferNotification
 
 
 class NotificationAPITest(TestCase):
+    url = "/api/offers/"
+
     def setUp(self):
         self.current_user = User.objects.create_user(username="username", email="test@test.com", password="password")
         self.current_user.userprofile.location = "location"
@@ -36,12 +39,17 @@ class NotificationAPITest(TestCase):
                             owner=self.other_user, category=c2)
 
     def post_offer(self, item_given, item_received, accepted=False, status=0, comment="test"):
-        return self.client.post("/api/offers/", data=json.dumps({
+        return self.client.post(self.url, data=json.dumps({
             "accepted": accepted,
             "status": status,
             "comment": comment,
             "item_given": item_given,
             "item_received": item_received
+        }), content_type="application/json")
+
+    def patch_offer(self, offer_id, accepted):
+        return self.client.patch(self.url + str(offer_id) + "/", data=json.dumps({
+            "accepted": accepted
         }), content_type="application/json")
 
     def test_new_offer_creation_notification(self):
@@ -51,8 +59,10 @@ class NotificationAPITest(TestCase):
         self.post_offer(2, 6)
         self.assertEqual(Notification.objects.count(), 2)
         self.assertEqual(Notification.objects.get(pk=1).user.username, "user1")
+        self.assertEqual(OfferNotification.objects.count(), 2)
         self.assertEqual(OfferNotification.objects.get(pk=1).offer.id, 1)
         self.assertEqual(OfferNotification.objects.get(pk=2).offer.id, 2)
+        self.assertEqual(NewOfferNotification.objects.count(), 2)
 
         self.client.logout()
         self.client.login(username="user1", password="password")
@@ -61,5 +71,45 @@ class NotificationAPITest(TestCase):
         self.post_offer(7, 1)
         self.assertEqual(Notification.objects.count(), 4)
         self.assertEqual(Notification.objects.get(pk=3).user.username, "username")
+        self.assertEqual(OfferNotification.objects.count(), 4)
         self.assertEqual(OfferNotification.objects.get(pk=3).offer.id, 3)
         self.assertEqual(OfferNotification.objects.get(pk=4).offer.id, 4)
+        self.assertEqual(NewOfferNotification.objects.count(), 4)
+
+    def test_offer_accepted_notification(self):
+        self.client.login(username="username", password="password")
+
+        self.post_offer(1, 8)
+        self.post_offer(2, 6)
+
+        r = self.patch_offer(1, True)
+        self.assertEqual(r.status_code, 200)
+
+        self.patch_offer(2, True)
+        self.assertEqual(r.status_code, 200)
+
+        self.assertEqual(Notification.objects.count(), 4)
+        self.assertEqual(Notification.objects.get(pk=1).user.username, "user1")
+        self.assertEqual(OfferNotification.objects.count(), 4)
+        self.assertEqual(OfferNotification.objects.get(pk=1).offer.id, 1)
+        self.assertEqual(OfferNotification.objects.get(pk=2).offer.id, 2)
+        self.assertEqual(AcceptedOfferNotification.objects.count(), 2)
+
+    def test_offer_refused_notification(self):
+        self.client.login(username="username", password="password")
+
+        self.post_offer(1, 8)
+        self.post_offer(2, 6)
+
+        r = self.patch_offer(1, False)
+        self.assertEqual(r.status_code, 200)
+
+        self.patch_offer(2, False)
+        self.assertEqual(r.status_code, 200)
+
+        self.assertEqual(Notification.objects.count(), 4)
+        self.assertEqual(Notification.objects.get(pk=1).user.username, "user1")
+        self.assertEqual(OfferNotification.objects.count(), 4)
+        self.assertEqual(OfferNotification.objects.get(pk=1).offer.id, 1)
+        self.assertEqual(OfferNotification.objects.get(pk=2).offer.id, 2)
+        self.assertEqual(RefusedOfferNotification.objects.count(), 2)
--- offers/views.py
+++ offers/views.py
@@ -1,4 +1,8 @@
+from django.utils import timezone
+from rest_framework import permissions
 from rest_framework import viewsets
+
+from notifications.models import Notification, OfferNotification, AcceptedOfferNotification, RefusedOfferNotification
 from offers.models import Offer
 from offers.serializers import OfferSerializer
 
@@ -6,6 +10,20 @@ from offers.serializers import OfferSerializer
 class OfferViewSet(viewsets.ModelViewSet):
     queryset = Offer.objects.all()
     serializer_class = OfferSerializer
+    permission_classes = (permissions.IsAuthenticated,)
 
-    #def get_queryset(self):
-        #return Offer.objects.filter(item_given__owner=self.request.user)
+    def perform_update(self, serializer):
+        offer = serializer.save()
+
+        if offer.accepted:
+            # create notification for accepted offer
+            notification = Notification.objects.create(content="Offer accepted for item: " + offer.item_received.name,
+                                                       read=False, date=timezone.now(), user=offer.item_received.owner)
+            offer_notification = OfferNotification.objects.create(notification=notification, offer=offer)
+            AcceptedOfferNotification.objects.create(offer_notification=offer_notification)
+        else:
+            # create notification for refused offer
+            notification = Notification.objects.create(content="Offer refused for item: " + offer.item_received.name,
+                                                       read=False, date=timezone.now(), user=offer.item_received.owner)
+            offer_notification = OfferNotification.objects.create(notification=notification, offer=offer)
+            RefusedOfferNotification.objects.create(offer_notification=offer_notification)
