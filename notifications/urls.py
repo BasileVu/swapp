21a5617b8532956e2353f1cc7dbb8921da75384d
sebastien_boson_heigvd <sebastien.boson@heig-vd.ch>
2017-01-02 23:12:36 +0100

    add notifications endpoint with get and patch methods

---
+++ notifications/urls.py
@@ -0,0 +1,7 @@
+from rest_framework.routers import DefaultRouter
+
+from . import views
+
+router = DefaultRouter()
+router.register(r'notifications', views.NotificationViewSet, base_name="notifications")
+urlpatterns = router.urls
--- notifications/views.py
+++ notifications/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework import mixins
+from rest_framework import permissions
+from rest_framework import viewsets
+from rest_framework.response import Response
 
-# Create your views here.
+from notifications.models import Notification
+from notifications.serializers import NotificationSerializer
+
+
+class NotificationViewSet(mixins.ListModelMixin,
+                          mixins.UpdateModelMixin,
+                          viewsets.GenericViewSet):
+    queryset = Notification.objects.all()
+    serializer_class = NotificationSerializer
+    permission_classes = (permissions.IsAuthenticated,)
+
+    def list(self, request, *args, **kwargs):
+        return Response(self.serializer_class(request.user.notification_set.order_by("date"), many=True).data)
