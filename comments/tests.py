8f4e5eb828497fe919fd0513ea4f50a496b06bcf
sebastien_boson_heigvd <sebastien.boson@heig-vd.ch>
2017-01-03 21:01:08 +0100

    add tests to check if an user can only see his own comments

--- comments/tests.py
+++ comments/tests.py
@@ -5,6 +5,7 @@ from django.test import TestCase
 from django.utils import timezone
 from rest_framework import status
 
+from comments.models import Comment
 from items.models import Item, Category
 
 
@@ -13,19 +14,16 @@ class CommentsTests(TestCase):
     url_comment1 = "%s/%d/" % (url_comments, 1)
 
     def setUp(self):
-        self.user = User.objects.create_user(username="username", password="password")
-        self.client.login(username="username", password="password")
+        self.user = User.objects.create_user(username="user1", password="password")
+        self.client.login(username="user1", password="password")
 
         self.category = Category.objects.create(name="category")
         self.item1 = Item.objects.create(name="test1", description="test", price_min=50, price_max=60,
                                          creation_date=timezone.now(), archived=False, owner=self.user,
                                          category=self.category)
 
-    def post_comment(self, item_id, content="test"):
-        return self.client.post(self.url_comments, data=json.dumps({
-            "content": content,
-            "item": item_id
-        }), content_type="application/json")
+    def create_comment(self, user, item, content="test"):
+        Comment.objects.create(content=content, user=user, item=item)
 
     def check_get_comment_data_complete(self, data):
         self.assertEqual(data["id"], 1)
@@ -35,12 +33,15 @@ class CommentsTests(TestCase):
         self.assertEqual(data["item"], self.item1.id)
 
     def test_post_comment(self):
-        r = self.post_comment(self.item1.id)
+        r = self.client.post(self.url_comments, data=json.dumps({
+            "content": "test",
+            "item": self.item1.id
+        }), content_type="application/json")
         self.assertEqual(r.status_code, status.HTTP_201_CREATED)
 
     def test_get_comments(self):
-        self.post_comment(self.item1.id)
-        self.post_comment(self.item1.id, "other test")
+        self.create_comment(self.user, self.item1)
+        self.create_comment(self.user, self.item1, "other test")
 
         r = self.client.get(self.url_comments)
         self.assertEqual(r.status_code, status.HTTP_200_OK)
@@ -48,18 +49,27 @@ class CommentsTests(TestCase):
         self.check_get_comment_data_complete(r.data[1])
 
     def test_get_comment(self):
-        self.post_comment(self.item1.id)
+        self.create_comment(self.user, self.item1)
 
         r = self.client.get(self.url_comment1)
         self.assertEqual(r.status_code, status.HTTP_200_OK)
         self.check_get_comment_data_complete(r.data)
 
+    def test_get_only_own_comments(self):
+        other_user = User.objects.create_user(username="user2", password="password")
+
+        self.create_comment(self.user, self.item1, "comment user1")
+        self.create_comment(other_user, self.item1, "comment user2")
+
+        r = self.client.get(self.url_comments)
+        self.assertEqual(len(r.data), 1)
+        self.assertEqual(r.data["content"], "comment user1")
+
     def test_put_comment(self):
         self.item2 = Item.objects.create(name="test2", description="test", price_min=50, price_max=60,
                                          creation_date=timezone.now(), archived=False, owner=self.user,
                                          category=self.category)
-
-        self.post_comment(self.item1.id)
+        self.create_comment(self.user, self.item1)
 
         r = self.client.put(self.url_comment1, data=json.dumps({
             "content": "put test",
@@ -72,7 +82,7 @@ class CommentsTests(TestCase):
         self.assertEqual(r.datat["item"], self.item2.id)
 
     def test_patch_comment(self):
-        self.post_comment(self.item1.id)
+        self.create_comment(self.user, self.item1)
 
         r = self.client.patch(self.url_comment1, data=json.dumps({
             "content": "patch test"
@@ -83,7 +93,7 @@ class CommentsTests(TestCase):
         self.assertEqual(r.data["content"], "patch test")
 
     def test_delete_comment(self):
-        self.post_comment(self.item1.id)
+        self.create_comment(self.user, self.item1)
 
         r = self.client.delete(self.url_comment1)
         self.assertEqual(r.status_code, status.HTTP_204_NO_CONTENT)
