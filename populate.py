0745e27c6306d10384d35468db8127b9591105c7
sebastien_boson_heigvd <sebastien.boson@heig-vd.ch>
2017-01-04 22:19:52 +0100

    update populate.py with offers and corresponding notifications

--- populate.py
+++ populate.py
@@ -1,7 +1,5 @@
 import os
 
-from notifications.models import Notification
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "swapp.settings")
 
 import django
@@ -11,6 +9,8 @@ django.setup()
 
 from users.models import *
 from items.models import *
+from comments.models import Comment
+from notifications.models import Notification
 
 
 def create_item(category, owner, name="Test", description="Test", price_min=1, price_max=2, archived=0):
@@ -18,9 +18,17 @@ def create_item(category, owner, name="Test", description="Test", price_min=1, p
                                price_max=price_max, archived=archived, category=category, owner=owner)
 
 
+def create_offer(item_given, item_received, comment="Offer test"):
+    return Offer.objects.create(item_given=item_given, item_received=item_received, comment=comment)
+
+
+def create_comment(user, item, content="Comment test"):
+    return Comment.objects.create(content=content, user=user, item=item)
+
+
 if __name__ == '__main__':
     # Users
-    u1 = User.objects.create_user(username="user1", email="test@test.com", password="password")
+    u1 = User.objects.create_user(username="user1", email="test1@test.com", password="password")
     u2 = User.objects.create_user(username="user2", email="test2@test.com", password="password")
     u3 = User.objects.create_user(username="user3", email="test3@test.com", password="password")
 
@@ -57,6 +65,7 @@ if __name__ == '__main__':
     c30 = Category.objects.create(name="Video Games & Consoles")
     c31 = Category.objects.create(name="Everything else")
 
+    # Items
     i1 = create_item(c8, u1, name="Shoes", description="My old shoes", price_min=10, price_max=30)
     i2 = create_item(c8, u1, name="Shirt", description="My old shirt", price_min=5, price_max=30)
     i3 = create_item(c19, u1, name="Ring", description="My precious", price_min=100, price_max=500)
@@ -68,4 +77,63 @@ if __name__ == '__main__':
     # image.save('test.png')
     # Image.objects.create(item=i1, image='test.png')
 
-    n1 = Notification.objects.create(content="First notification on a new offer", read=False)
+    # Create new offers with corresponding notifications (made automatically)
+    o1 = create_offer(i1, i4)
+    o2 = create_offer(i2, i5)
+    o3 = create_offer(i3, i4)
+    o4 = create_offer(i5, i2)
+
+    # Change read to True for new offers notifications 2 and 4
+    n2 = Notification.objects.get(pk=2)
+    n2.read = True
+    n2.save()
+    n4 = Notification.objects.get(pk=4)
+    n4.read = True
+    n4.save()
+
+    # Change status and accepted values for offers with the creation of corresponding notifications (made
+    # automatically)
+    o1.status = True
+    o1.accepted = True
+    o1.save()
+    o2.status = True
+    o2.accepted = True
+    o2.save()
+    o3.status = True
+    o3.accepted = False
+    o3.save()
+    o4.status = True
+    o4.accepted = True
+    o4.save()
+
+    # Change read to True for new accepted offer notification 5 and for new refused offer notification 7
+    # n5 = Notification.objects.get(pk=5)
+    # n5.read = True
+    # n5.save()
+    # n7 = Notification.objects.get(pk=7)
+    # n7.read = True
+    # n7.save()
+
+    # Create new comments with corresponding notifications (made automatically)
+    # co1 = create_comment(u1, i4)
+    # co2 = create_comment(u1, i5)
+    # co3 = create_comment(u2, i1)
+    # co4 = create_comment(u2, i2)
+    #
+    # # Change read to True for new comments notifications 9 and 10
+    # n9 = Notification.objects.get(pk=9)
+    # n9.read = True
+    # n9.save()
+    # n10 = Notification.objects.get(pk=10)
+    # n10.read = True
+    # n10.save()
+    #
+    # # Create new notes with corresponding notifications (made automatically)
+    # no1 = Note.objects.create(user=u2, offer=o1, text="Very good", note=5)
+    # no2 = Note.objects.create(user=u2, offer=o2, text="Very bad", note=0)
+    # no3 = Note.objects.create(user=u1, offer=o4, text="Not bad", note=2)
+    #
+    # # Change read to True for new note notification 14
+    # n13 = Notification.objects.get(pk=14)
+    # n13.read = True
+    # n13.save()
