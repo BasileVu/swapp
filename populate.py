a937f5de29058975a502702af37da776b2682e24
sebastien_boson_heigvd <sebastien.boson@heig-vd.ch>
2017-01-05 23:45:35 +0100

    add locations and coordinates for users in populate.py

--- populate.py
+++ populate.py
@@ -5,7 +5,6 @@ from django.core.files import File
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "swapp.settings")
 
 import django
-from PIL import Image as ImagePil
 
 django.setup()
 
@@ -46,6 +45,48 @@ if __name__ == "__main__":
     u3 = User.objects.create_user(username="user3", email="test3@test.com", password="password")
     u4 = User.objects.create_user(username="user4", email="test4@test.com", password="password")
 
+    # Add locations to users
+    u1.location.street = "Route de Cheseaux 1"
+    u1.location.city = "Yverdon-les-Bains"
+    u1.location.region = "VD"
+    u1.location.country = "Switzerland"
+    u1.location.save()
+
+    u2.location.street = "Avenue des Sports 20"
+    u2.location.city = "Yverdon-les-Bains"
+    u2.location.region = "VD"
+    u2.location.country = "Switzerland"
+    u2.location.save()
+
+    u3.location.street = "Place Pestalozzi 14"
+    u3.location.city = "Yverdon-les-Bains"
+    u3.location.region = "VD"
+    u3.location.country = "Switzerland"
+    u3.location.save()
+
+    u4.location.street = "385 Route de Meyrin"
+    u4.location.city = "Meyrin"
+    u4.location.region = "GE"
+    u4.location.country = "Switzerland"
+    u4.location.save()
+
+    # Add coordinates to users
+    u1.coordinates.latitude = 46.7793801
+    u1.coordinates.longitude = 6.659497600000001
+    u1.coordinates.save()
+
+    u2.coordinates.latitude = 46.7812274
+    u2.coordinates.longitude = 6.6473097
+    u2.coordinates.save()
+
+    u3.coordinates.latitude = 46.77866239999999
+    u3.coordinates.longitude = 6.6419655
+    u3.coordinates.save()
+
+    u4.coordinates.latitude = 46.2337481
+    u4.coordinates.longitude = 6.0523948
+    u4.coordinates.save()
+
     # Categories
     c1 = Category.objects.create(name="Antiques")
     c2 = Category.objects.create(name="Art")
@@ -83,13 +124,16 @@ if __name__ == "__main__":
     i1 = create_item(c8, u1, name="Shoes", description="My collector shoes", price_min=10, price_max=50, views=800)
     i2 = create_item(c8, u1, name="Shirt", description="My old shirt", price_min=5, price_max=30, views=100)
     i3 = create_item(c19, u1, name="Ring", description="My precious", price_min=100, price_max=500, views=400)
+
     i4 = create_item(c12, u2, name="New mouse", description="Brand new", price_min=40, price_max=150, views=200)
     i5 = create_item(c22, u2, name="Piano", description="Still nice to the ear", price_min=500, price_max=1000,
                      views=1000)
+
     i6 = create_item(c22, u3, name="Violin", description="In good shape", price_min=1000, price_max=2000, views=30)
     i7 = create_item(c22, u3, name="Flute", description="Very old", price_min=200, price_max=300, views=40)
     i8 = create_item(c22, u3, name="Electric guitar", description="One cord is broken", price_min=150, price_max=250,
                      views=50)
+
     i9 = create_item(c22, u4, name="Trumpet", description="Good sound", price_min=300, price_max=350, views=60)
 
     # Set userprofiles images
@@ -102,20 +146,28 @@ if __name__ == "__main__":
     set_image_item(i1, "shoes_1.jpg")
     set_image_item(i1, "shoes_2.jpg")
     set_image_item(i1, "shoes_3.jpg")
+
     set_image_item(i2, "shirt_1.jpg")
     set_image_item(i2, "shirt_2.jpg")
+
     set_image_item(i3, "ring_1.jpg")
+
     set_image_item(i4, "newMouse_1.jpg")
     set_image_item(i4, "newMouse_2.png")
     set_image_item(i4, "newMouse_3.jpg")
+
     set_image_item(i5, "piano_1.png")
     set_image_item(i5, "piano_2.jpg")
+
     set_image_item(i6, "violin_1.jpg")
+
     set_image_item(i7, "flute_1.jpg")
     set_image_item(i7, "flute_2.jpg")
     set_image_item(i7, "flute_3.jpg")
+
     set_image_item(i8, "electricGuitar_1.jpg")
     set_image_item(i8, "electricGuitar_2.jpg")
+
     set_image_item(i9, "trumpet_1.jpg")
     set_image_item(i9, "trumpet_2.jpg")
     set_image_item(i9, "trumpet_3.jpg")
@@ -124,27 +176,35 @@ if __name__ == "__main__":
     KeyInfo.objects.create(key="Brand", info="Adidum", item=i1)
     KeyInfo.objects.create(key="Size", info="40", item=i1)
     KeyInfo.objects.create(key="Color", info="Purple", item=i1)
+
     KeyInfo.objects.create(key="Brand", info="Niko", item=i2)
     KeyInfo.objects.create(key="Size", info="L", item=i2)
     KeyInfo.objects.create(key="Color", info="White", item=i2)
+
     KeyInfo.objects.create(key="Made in", info="Mordor", item=i3)
     KeyInfo.objects.create(key="Manufacturer", info="Sauron", item=i3)
     KeyInfo.objects.create(key="Color", info="Golden", item=i3)
+
     KeyInfo.objects.create(key="Brand", info="LogicTe", item=i4)
     KeyInfo.objects.create(key="Hand", info="Left", item=i4)
     KeyInfo.objects.create(key="Color", info="Black and white", item=i4)
+
     KeyInfo.objects.create(key="Brand", info="StenStay", item=i5)
     KeyInfo.objects.create(key="Hand", info="Left and right", item=i5)
     KeyInfo.objects.create(key="Color", info="Black", item=i5)
+
     KeyInfo.objects.create(key="Brand", info="StradiWarior", item=i6)
     KeyInfo.objects.create(key="Mater", info="Wood", item=i6)
     KeyInfo.objects.create(key="Quality", info="Brand new", item=i6)
+
     KeyInfo.objects.create(key="Brand", info="Pan", item=i7)
     KeyInfo.objects.create(key="Quality", info="Bad shape", item=i7)
     KeyInfo.objects.create(key="Color", info="White", item=i7)
+
     KeyInfo.objects.create(key="Brand", info="Power", item=i8)
     KeyInfo.objects.create(key="Manufacturer", info="Elvis", item=i8)
     KeyInfo.objects.create(key="Color", info="Blue ocean", item=i8)
+
     KeyInfo.objects.create(key="Sound", info="Pouette", item=i9)
     KeyInfo.objects.create(key="Quality", info="Good shape", item=i9)
     KeyInfo.objects.create(key="Made in", info="China", item=i9)
@@ -153,19 +213,27 @@ if __name__ == "__main__":
     Like.objects.create(user=u2, item=i1)
     Like.objects.create(user=u3, item=i1)
     Like.objects.create(user=u4, item=i1)
+
     Like.objects.create(user=u2, item=i2)
     Like.objects.create(user=u3, item=i2)
+
     Like.objects.create(user=u3, item=i3)
+
     Like.objects.create(user=u1, item=i4)
     Like.objects.create(user=u3, item=i4)
     Like.objects.create(user=u4, item=i4)
+
     Like.objects.create(user=u4, item=i5)
     Like.objects.create(user=u1, item=i5)
+
     Like.objects.create(user=u4, item=i6)
+
     Like.objects.create(user=u4, item=i7)
     Like.objects.create(user=u1, item=i7)
     Like.objects.create(user=u2, item=i7)
+
     Like.objects.create(user=u2, item=i8)
+
     Like.objects.create(user=u3, item=i9)
     Like.objects.create(user=u1, item=i9)
 
@@ -180,6 +248,7 @@ if __name__ == "__main__":
     n2 = Notification.objects.get(pk=2)
     n2.read = True
     n2.save()
+
     n4 = Notification.objects.get(pk=4)
     n4.read = True
     n4.save()
@@ -189,12 +258,15 @@ if __name__ == "__main__":
     o1.answered = True
     o1.accepted = True
     o1.save()
+
     o2.answered = True
     o2.accepted = True
     o2.save()
+
     o3.answered = True
     o3.accepted = False
     o3.save()
+
     o4.answered = True
     o4.accepted = True
     o4.save()
@@ -203,6 +275,7 @@ if __name__ == "__main__":
     n6 = Notification.objects.get(pk=6)
     n6.read = True
     n6.save()
+
     n8 = Notification.objects.get(pk=8)
     n8.read = True
     n8.save()
@@ -210,16 +283,20 @@ if __name__ == "__main__":
     # Create new comments with corresponding notifications (made automatically)
     co1 = create_comment(u1, i4, "I think this mouse is not a good deal.")
     co2 = create_comment(u1, i5, "In my opinion, a piano can't be traded.")
+
     co3 = create_comment(u2, i1, "Oh, this old shoes are so rare.")
     co4 = create_comment(u2, i2, "Seriously, i don't think we can trade an old shirt. That is too ugly.")
+
     co5 = create_comment(u3, i1, "Aren't those the shoes of a famous football player?")
     co6 = create_comment(u3, i1, "But still, those shoes look smelly.")
+
     co7 = create_comment(u4, i1, "I want to put those shoes on.")
 
     # Change read to True for new comments notifications 10 and 11
     n10 = Notification.objects.get(pk=10)
     n10.read = True
     n10.save()
+
     n11 = Notification.objects.get(pk=11)
     n11.read = True
     n11.save()
@@ -227,6 +304,7 @@ if __name__ == "__main__":
     # Create new notes with corresponding notifications (made automatically)
     no1 = Note.objects.create(user=u2, offer=o1, text="Very good", note=5)
     no2 = Note.objects.create(user=u2, offer=o2, text="Very bad", note=0)
+
     no3 = Note.objects.create(user=u1, offer=o4, text="Not too bad", note=3)
 
     # Change read to True for new note notification 17
