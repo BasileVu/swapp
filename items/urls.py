20bfb7270454883a001c2f132a6f8eb92ea76e02
sebastien_boson_heigvd <sebastien.boson@heig-vd.ch>
2017-01-06 21:44:24 +0100

    add endpoint for get all delivery_methods

--- items/urls.py
+++ items/urls.py
@@ -7,4 +7,5 @@ router.register(r"items", views.ItemViewSet, base_name="items")
 router.register(r"categories", views.CategoryViewSet, base_name="categories")
 router.register(r"likes", views.LikeViewSet, base_name="likes")
 router.register(r"images", views.ImageViewSet, base_name="images")
+router.register(r"deliverymethods", views.DeliveryMethodViewSet, base_name="delivery_methods")
 urlpatterns = router.urls
