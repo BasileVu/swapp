f081be5ac15d2c4ee2d8a8eda815af4b6f4940f4
Basile Vu <basile.vu@gmail.com>
2017-01-08 14:47:42 +0100

    Updated messages serializer and view

--- private_messages/serializers.py
+++ private_messages/serializers.py
@@ -4,6 +4,9 @@ from private_messages.models import Message
 
 
 class MessageSerializer(serializers.ModelSerializer):
+    user_from = serializers.PrimaryKeyRelatedField(read_only=True)
+    date = serializers.DateTimeField(read_only=True)
+
     class Meta:
         model = Message
-        fields = ('text', 'date', 'user_from', 'user_to')
+        fields = ('user_from', 'user_to', 'text', 'date')
