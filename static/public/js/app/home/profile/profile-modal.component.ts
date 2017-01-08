736519b42b1ee5ce24a105bc8012809a00001f1b
Mathieu Urstein <mathieu.urstein@gmail.com>
2017-01-08 22:02:45 +0100

    Remove notifications logs

--- static/public/js/app/home/profile/profile-modal.component.ts
+++ static/public/js/app/home/profile/profile-modal.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit, ViewEncapsulation} from '@angular/core';
 import {AuthService} from "../../shared/authentication/authentication.service";
 import {Subscription} from "rxjs";
+import {Account} from "./account";
 
 @Component({
     moduleId: module.id,
@@ -12,6 +13,7 @@ import {Subscription} from "rxjs";
 export class ProfileModalComponent implements OnInit {
 
     private loggedIn: boolean;
+    user: Account;
     subscription: Subscription;
 
     constructor(private authService : AuthService) {}
@@ -19,6 +21,12 @@ export class ProfileModalComponent implements OnInit {
     ngOnInit() {
         this.subscription = this.authService.loggedInSelected$.subscribe(
             loggedIn => this.loggedIn = loggedIn
+        );
+        this.authService.getAccount().then(
+            account => {
+                this.user = account;
+                console.log(account);
+            }
         );
     }
 }
