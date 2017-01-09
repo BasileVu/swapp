123eb4d629c3290acedd5b2133d02f1c88a8df2d
Mathieu Urstein <mathieu.urstein@gmail.com>
2017-01-09 13:03:37 +0100

    Put the search map control in the right file

--- static/public/js/app/home/search/search-modal.component.ts
+++ static/public/js/app/home/search/search-modal.component.ts
@@ -6,6 +6,9 @@ import {OrderBy} from "./orderby";
 import {ItemsService} from "../items/items.service";
 import {Search} from "./search";
 
+declare let $: any;
+declare let google: any;
+
 @Component({
     moduleId: module.id,
     selector: 'search-modal',
@@ -36,6 +39,39 @@ export class SearchModalComponent {
     ngOnInit() {
         this.getCategories();
         this.model.range = '100';
+
+        let advancedSearchModal = $('#advanced-search-modal');
+        advancedSearchModal.on('show.bs.modal', function (e: any) {
+            setTimeout(function () {
+                let map = new google.maps.Map(document.getElementById('search-modal-map'), {
+                    center: {lat: -34.397, lng: 150.644},
+                    scrollwheel: false,
+                    zoom: 8
+                });
+                new google.maps.Marker({
+                    map: map,
+                    position: {lat: -34.197, lng: 150.844}
+                });
+                new google.maps.Marker({
+                    map: map,
+                    position: {lat: -34.308, lng: 150.679},
+                });
+                new google.maps.Marker({
+                    map: map,
+                    position: {lat: -34.390, lng: 150.664}
+                });
+                new google.maps.Circle({
+                    map: map,
+                    center: {lat: -34.397, lng: 150.644},
+                    radius: 100000,    // 10 miles in metres
+                    fillColor: '#eed5a9',
+                    fillOpacity: 0.3,
+                    strokeColor: '#40b2cd',
+                    strokeOpacity: 1,
+                    strokeWeight: 3
+                });
+            }, 300)
+        });
     }
 
     selectCategory(category: Category){
--- static/public/js/custom.js
+++ static/public/js/custom.js
@@ -190,35 +190,4 @@ $('document').ready(function() {
     $('.open-modal-advanced-search').click(function () {
         advancedSearchModal.modal('show');
     });
-    advancedSearchModal.on('show.bs.modal', function (e) {
-        setTimeout(function () {
-            var map = new google.maps.Map(document.getElementById('search-modal-map'), {
-                center: {lat: -34.397, lng: 150.644},
-                scrollwheel: false,
-                zoom: 8
-            });
-            new google.maps.Marker({
-                map: map,
-                position: {lat: -34.197, lng: 150.844}
-            });
-            new google.maps.Marker({
-                map: map,
-                position: {lat: -34.308, lng: 150.679},
-            });
-            new google.maps.Marker({
-                map: map,
-                position: {lat: -34.390, lng: 150.664}
-            });
-            new google.maps.Circle({
-                map: map,
-                center: {lat: -34.397, lng: 150.644},
-                radius: 100000,    // 10 miles in metres
-                fillColor: '#eed5a9',
-                fillOpacity: 0.3,
-                strokeColor: '#40b2cd',
-                strokeOpacity: 1,
-                strokeWeight: 3
-            });
-        }, 300)
-    });
 });
