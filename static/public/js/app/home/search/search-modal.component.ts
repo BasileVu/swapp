97286123d365c564b5b150184c4d5a39d1693423
Mathieu Urstein <mathieu.urstein@gmail.com>
2017-01-11 03:12:57 +0100

    Search position

--- static/public/js/app/home/search/search-modal.component.ts
+++ static/public/js/app/home/search/search-modal.component.ts
@@ -33,6 +33,8 @@ export class SearchModalComponent {
     ];
     model: Search = new Search();
     map: any;
+    markers: any[] = [];
+    circle: any;
     center: any = {lat: 0, lng: 0};
     advancedSearchModal: any;
     searchLocation: string = '';
@@ -42,7 +44,7 @@ export class SearchModalComponent {
         private itemsService: ItemsService,
         private authService: AuthService,
         private googleService: GoogleService
-    ) {}
+    ) { }
 
     ngOnInit() {
         let that = this;
@@ -56,8 +58,8 @@ export class SearchModalComponent {
 
         this.authService.getAccount().then(res => {
             this.searchLocation = res.location.street + ', ' +
-                                    res.location.city + ', ' +
-                                    res.location.country;
+                res.location.city + ', ' +
+                res.location.country;
         });
         this.advancedSearchModal = $('#advanced-search-modal');
         this.advancedSearchModal.on('show.bs.modal', function (e: any) {
@@ -65,58 +67,87 @@ export class SearchModalComponent {
                 that.map = new google.maps.Map(document.getElementById('search-modal-map'), {
                     scrollwheel: false
                 });
+                that.circle = new google.maps.Circle({
+                    map: that.map,
+                    center: that.center,
+                    radius: 100000,    // 10 miles in metres
+                    fillColor: '#eed5a9',
+                    fillOpacity: 0.3,
+                    strokeColor: '#40b2cd',
+                    strokeOpacity: 1,
+                    strokeWeight: 3
+                });
                 that.setLocation();
             }, 300)
         });
     }
 
-    setLocation() {
+    private setLocation() {
+        this.deleteMarkers();
+
         let url = 'https://maps.googleapis.com/maps/api/geocode/json?address='+this.searchLocation+'&key=AIzaSyDNi0DkJRcQiOhJzSitoV5GhlacK6fNtKs';
 
-        this.googleService.find(url).then(
-            res => {
-                let data: any = this.extractData(res);
-                this.center = data.results[0].geometry.location;
+        if (this.searchLocation.length > 3) {
+            this.googleService.find(url).then(
+                res => {
+                    if (typeof res !== "undefined" ) {
+                        let data: any = this.extractData(res);
 
-                this.recenterMap();
-            },
-            error => this.handleError(error)
-        );
-
-        this.addMarker({lat: -34.197, lng: 150.844});
-        this.addMarker({lat: -34.308, lng: 150.679});
-        this.addMarker({lat: -34.390, lng: 150.664});
-        this.addCircle(10000);
+                        if( typeof data.results[0] !== 'undefined') {
+                            this.center = data.results[0].geometry.location;
+                        this.recenterMap();
+                        }
+                    }
+                },
+                error => this.handleError(error)
+            );
+        }
     }
 
-    recenterMap() {
+    private recenterMap() {
         switch(this.model.range) {
-            case'1': {
+            case '1': {
+                this.map.setZoom(15);
+                break;
+            }
+            case '10': {
+                this.map.setZoom(12);
+                break;
+            }
+            case '50': {
+                this.map.setZoom(10);
+                break;
+            }
+            case '100': {
                 this.map.setZoom(9);
                 break;
             }
-            case'10': {
-                this.map.setZoom(8);
-                break;
-            }
-            case'50': {
-                this.map.setZoom(7);
-                break;
-            }
-            case'100': {
+            case '500': {
                 this.map.setZoom(6);
                 break;
             }
-            case'500': {
-                this.map.setZoom(5);
-                break;
-            }
-            case'0': {
+            case '0': {
                 this.map.setZoom(2);
                 break;
             }
         }
+        this.changeCircle(parseInt(this.model.range) * 1000);
         this.map.setCenter(this.center);
+    }
+
+    private setMapOnAll(map: any) {
+        for (let i = 0; i < this.markers.length; i++) {
+            this.markers[i].setMap(map);
+        }
+    }
+
+    private clearMarkers() {
+        this.setMapOnAll(null);
+    }
+
+    private deleteMarkers() {
+        this.clearMarkers();
+        this.markers = [];
     }
 
     private extractData(res: Response) {
@@ -138,23 +169,15 @@ export class SearchModalComponent {
     }
 
     addMarker(positions: any) {
-        new google.maps.Marker({
+        this.markers.push(new google.maps.Marker({
             map: this.map,
             position: positions
-        });
+        }));
     }
 
-    addCircle(radius: number) {
-        new google.maps.Circle({
-            map: this.map,
-            center: this.center,
-            radius: radius,    // 10 miles in metres
-            fillColor: '#eed5a9',
-            fillOpacity: 0.3,
-            strokeColor: '#40b2cd',
-            strokeOpacity: 1,
-            strokeWeight: 3
-        });
+    private changeCircle(radius: number) {
+        this.circle.setRadius(radius/2);
+        this.circle.setCenter(this.center);
     }
 
     ngOnDestroy() {
@@ -176,13 +199,23 @@ export class SearchModalComponent {
         this.searchService.model.next(this.model);
     }
 
+    newLocation(pos: string) {
+        this.searchLocation = pos;
+        this.setLocation();
+    }
+
+    selectRange(value: any) {
+        this.model.range = value;
+        this.searchService.model.next(this.model);
+    }
+
     getCategories() {
         this.searchService.getCategories()
             .then(
-                 categories => {
-                     this.categories = categories;
-                     this.categories.unshift(new Category("All categories"));
-                 },
+                categories => {
+                    this.categories = categories;
+                    this.categories.unshift(new Category("All categories"));
+                },
                 error =>  this.errorMessage = <any>error);
     }
 
