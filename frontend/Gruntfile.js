58e0d9a5725339feed795ecf714fe817eac96787
Basile Vu <basile.vu@gmail.com>
2017-03-14 18:01:09 +0100

    Beginning modifying Gruntfile

--- frontend/Gruntfile.js
+++ frontend/Gruntfile.js
@@ -1,3 +1,6 @@
+var STATIC_PATH = "../backend/static/public/";
+var VENDOR_PATH = STATIC_PATH + "js/vendor/";
+
 module.exports = function(grunt) {
     grunt.initConfig({
         copy: {
@@ -5,64 +8,65 @@ module.exports = function(grunt) {
                 files: [
                     {
                         expand: true,
-                        src: ['node_modules/font-awesome/fonts/*'],
-                        dest: 'static/public/fonts/',
+                        src: ["node_modules/font-awesome/fonts/*"],
+                        dest: STATIC_PATH + "fonts/",
                         flatten: true
                     }, {
                         expand: true,
                         src: [
-                            'node_modules/tether/dist/js/tether.js',
-                            'node_modules/imagesloaded/imagesloaded.pkgd.js',
-                            'node_modules/jquery/dist/jquery.js',
-                            'node_modules/bootstrap/dist/js/bootstrap.js',
-                            'node_modules/isotope-layout/dist/isotope.pkgd.js',
-                            'node_modules/flickity/dist/flickity.pkgd.js',
-                            'node_modules/jquery-match-height/dist/jquery.matchHeight.js',
-                            'node_modules/zone.js/dist/zone.js',
-                            'node_modules/reflect-metadata/Reflect.js',
-                            'node_modules/systemjs/dist/system.src.js'
+                            "node_modules/tether/dist/js/tether.js",
+                            "node_modules/imagesloaded/imagesloaded.pkgd.js",
+                            "node_modules/jquery/dist/jquery.js",
+                            "node_modules/bootstrap/dist/js/bootstrap.js",
+                            "node_modules/isotope-layout/dist/isotope.pkgd.js",
+                            "node_modules/flickity/dist/flickity.pkgd.js",
+                            "node_modules/jquery-match-height/dist/jquery.matchHeight.js",
+                            "node_modules/zone.js/dist/zone.js",
+                            "node_modules/reflect-metadata/Reflect.js",
+                            "node_modules/systemjs/dist/system.src.js"
                         ],
-                        dest: 'static/public/js/vendor/',
+                        dest: VENDOR_PATH,
                         flatten: true
                     }, {
                         expand: true,
-                        cwd: 'node_modules/@angular/',
-                        src: ['**'],
-                        dest: 'static/public/js/vendor/@angular/'
+                        cwd: "node_modules/@angular/",
+                        src: ["**"],
+                        dest: VENDOR_PATH + "@angular/"
                     }, {
                         expand: true,
-                        cwd: 'node_modules/rxjs/',
-                        src: ['**'],
-                        dest: 'static/public/js/vendor/rxjs/'
+                        cwd: "node_modules/rxjs/",
+                        src: ["**"],
+                        dest: VENDOR_PATH + "rxjs/"
                     }, {
                         expand: true,
-                        cwd: 'node_modules/ng2-img-cropper/',
-                        src: ['**'],
-                        dest: 'static/public/js/vendor/ng2-img-cropper/'
+                        cwd: "node_modules/ng2-img-cropper/",
+                        src: ["**"],
+                        dest: VENDOR_PATH + "ng2-img-cropper/"
                     }, {
                         expand: true,
-                        cwd: 'node_modules/ng2-toastr/',
-                        src: ['**'],
-                        dest: 'static/public/js/vendor/ng2-toastr/'
+                        cwd: "node_modules/ng2-toastr/",
+                        src: ["**"],
+                        dest: VENDOR_PATH + "ng2-toastr/"
                     }, {
                         expand: true,
-                        cwd: 'node_modules/ng2-rating/',
-                        src: ['**'],
-                        dest: 'static/public/js/vendor/ng2-rating/'
+                        cwd: "node_modules/ng2-rating/",
+                        src: ["**"],
+                        dest: VENDOR_PATH + "ng2-rating/"
                     }
                 ]
             }
         },
         sass: {
             dist: {
-                files: {
-                    'static/public/css/styles.css': 'static/scss/styles.scss'
-                }
+                files: [{ 
+                    dest: STATIC_PATH + "css/styles.css", 
+                    src: "scss/styles.scss" 
+                }]
             }
         },
         ts: {
             default: {
-                src: ["static/public/js/app/**/*.ts", "!node_modules/**"],
+                src: [STATIC_PATH + "js/app/**/*.ts", "!node_modules/**"],
                 options: {
                     "target": "es5",
                     "module": "commonjs",
@@ -77,18 +81,18 @@ module.exports = function(grunt) {
         },
         watch: {
             styles: {
-                files: ['static/scss/**/*.scss'],
-                tasks: ['sass']
+                files: ["scss/*.scss"],
+                tasks: ["sass"]
             },
             typescripts: {
-                files: ['static/**/*.ts'],
-                tasks: ['ts']
+                files: [STATIC_PATH + "**/*.ts"],
+                tasks: ["ts"]
             },
             livereload: {
                 files : [
-                    '**/templates/**/*',
-                    '**/static/public/**/*',
-                    '**/*.py'
+                    "**/templates/**/*",
+                    "**/static/public/**/*",
+                    "**/*.py"
                 ],
                 options: {
                     livereload: 8080
@@ -97,15 +101,15 @@ module.exports = function(grunt) {
         }
     });
 
-    grunt.loadNpmTasks('grunt-contrib-copy');
-    grunt.loadNpmTasks('grunt-sync');
-    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks("grunt-contrib-copy");
+    grunt.loadNpmTasks("grunt-sync");
+    grunt.loadNpmTasks("grunt-contrib-sass");
     grunt.loadNpmTasks("grunt-ts");
-    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks("grunt-contrib-watch");
 
     // Default task(s).
-    grunt.registerTask('default', ['copy', 'sass', 'ts', 'watch']);
+    grunt.registerTask("default", ["copy", "sass", "ts", "watch"]);
 
     // Build without watch
-    grunt.registerTask('build', ['copy', 'sass', 'ts']);
+    grunt.registerTask("build", ["copy", "sass", "ts"]);
 };
